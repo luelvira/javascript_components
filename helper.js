36ea549e3bef9dfc6e6fdbc7b3dab7908c569620
Lucas Elvira Martín <lucaselvira96@gmail.com>
2022-04-21 09:59:13 +0200

    Add some common functions

---
+++ helper.js
@@ -0,0 +1,45 @@
+$ = function(selector) {
+	let result = document.querySelectorAll(selector);
+	return selector[0] === '#' ? result[0] : result;
+}
+
+/* prototype helpers */
+
+HTMLElement.prototype.on = function(type, callback) {
+	type.split(" ").forEach(eType => this.addEventListener(eType, callback));
+	return this;
+}
+
+HTMLElement.prototype.attr = function(name, value) {
+	if (typeof(value)  != 'undefined')  {
+		this.setAttribute(name, value);
+		return this;
+	} 
+	return this.getAttribute(name);
+}
+
+HTMLElement.prototype.rattr = function(name) {
+	this.removeAttribute(name);
+}
+
+NodeList.prototype.on = function(type, callback) {
+	for (let node of this)
+		node.on(type, callback);
+	return this;
+}
+
+NodeList.prototype.each = function(callback) {
+	this.forEach(callback)
+	return this;
+}
+
+NodeList.prototype.attr = function(name, value) {
+	for (let node of this)
+		node.attr(name, value);
+	return this;
+}
+
+function preventDefault(event) {
+	event.preventDefault();
+	event.stopPropagation();
+}
