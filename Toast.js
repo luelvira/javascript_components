63fa2f6809dea36047dd3a2c0ba5715d7ffd9d91
Lucas Elvira Martín <lucaselvira96@gmail.com>
2023-03-30 18:01:22 +0200

    Update toast

--- Toast.js
+++ Toast.js
@@ -8,120 +8,124 @@
  * @updated 2022-11
  * @link https://github.com/luelvira/javascript_components
  */
-(function() {
-	'use strict';
 
-	const ToastConstants = {
-		BOTTOM: true,
-		TOP: false,
-		DEFAULT_DURATION: 2000
-	};
+const ToastConstants = {
+    BOTTOM: true,
+    TOP: false,
+    DEFAULT_DURATION: 2000,
+};
 
-	const setStyle = function() {
-		return `
+function setStyle() {
+    return `
 #toast {
-	background-color: ${this._bgColor};
-	position: fixed;
-	width: 80%;
-	left: 10%;
-	text-align: center;
-	padding: 2% 0;
-	box-sizing: border-box;
-	border-radius: 25px;
-	box-shadow: 0 3px 10px #0e0e0e;
-	opacity: 0.95;
-	z-index: 99;
-	${(()=> this._position ? "bottom: 2%;" : "top: 2%;")()}
-	${(()=> this._color ? `color: ${this._color};`: "")()}
+    background-color: ${this._bgColor};
+    position: fixed;
+    width: 80%;
+    left: 10%;
+    text-align: center;
+    padding: 2% 0;
+    box-sizing: border-box;
+    border-radius: 25px;
+    box-shadow: 0 3px 10px #0e0e0e;
+    opacity: 0.95;
+    z-index: 99;
+    ${(() => (this._position ? "bottom: 2%;" : "top: 2%;"))()}
+    ${(() => (this._color ? `color: ${this._color};` : ""))()}
 }
 #toast.fadein { animation: fadein 1s ease; }
 #toast.fadeout { animation: fadeout 1s ease; }
 
 @keyframes fadein {
-	from { opacity: 0;}
-	to {opacity: 0.95;}
+    from { opacity: 0;}
+    to {opacity: 0.95;}
 }
 
 @keyframes fadeout {
-	from { opacity: 0.95;}
-	to {opacity: 0;}
+    from { opacity: 0.95;}
+    to {opacity: 0;}
 }
 `;
-	}
+}
 
-	const Toast = function() {
-		const container = document.createElement("div");
-		const toast = document.createElement("div");
-		toast.setAttribute("id", "toast");
-		toast.style.display = "none";
-		const shadow = container.attachShadow({mode: 'open'});
-		this.style = document.createElement("style");
-		this.style.textContent = "";
-		shadow.append(this.style, toast);
-		document.body.appendChild(container)
-		this.toastElement = toast;
-		const totalHidden = () => {
-			if (!toast.classList.contains("fadein"))
-				this.toastElement.style.display = "none";
-		}
-		toast.addEventListener("webkitAnimationEnd", () => totalHidden());
-		toast.addEventListener("animationEnd", ()=> totalHidden());
-		toast.addEventListener("msAnimationEnd", ()=> totalHidden());
-		toast.addEventListener("oAnimationEnd", ()=>totalHidden());
-	}
+function hide() {
+    this.toastElement.classList.remove("fadein");
+    this.toastElement.classList.add("fadeout");
+}
 
-	Toast.__defineGetter__("BOTTOM", function() {return ToastConstants.BOTTOM});
-	Toast.__defineGetter__("TOP", function() {return ToastConstants.TOP});
-	Toast.__defineGetter__("DEFAULT_DURATION", function() {return ToastConstants.DEFAULT_DURATION});
+export class Toast {
+    static toast;
 
-	Toast.prototype.__defineSetter__("duration", function(t) {
-		if (t <= 0) throw "Error: Toast duration must be a positive integer";
-		this._duration = parseInt(t); 
-	});
+    constructor() {
+        const container = document.createElement("div");
+        const toast = document.createElement("div");
+        toast.setAttribute("id", "toast");
+        toast.style.display = "none";
+        const shadow = container.attachShadow({ mode: "open" });
+        this.style = document.createElement("style");
+        this.style.textContent = "";
+        shadow.append(this.style, toast);
+        document.body.appendChild(container);
+        this.toastElement = toast;
+        const totalHidden = () => {
+            if (!toast.classList.contains("fadein"))
+                this.toastElement.style.display = "none";
+        };
+        toast.addEventListener("webkitAnimationEnd", () => totalHidden());
+        toast.addEventListener("animationEnd", () => totalHidden());
+        toast.addEventListener("msAnimationEnd", () => totalHidden());
+        toast.addEventListener("oAnimationEnd", () => totalHidden());
+    }
 
-	Toast.prototype.__defineSetter__("position", function(pos) {
-		if (pos !== ToastConstants.BOTTOM && pos !== ToastConstants.TOP) 
-			throw "Error: Toast position must be equal to Toast.BOTTOM or Toast.TOP";
-		this._position = pos;
-		this.style.textContent = setStyle.call(this);
-	});
+    static get BOTTOM() {
+        return ToastConstants.BOTTOM;
+    }
 
-	Toast.prototype.__defineSetter__("text", function(text) {
-		this.toastElement.innerText = text || "";
-	});
+    static get TOP() {
+        return ToastConstants.TOP;
+    }
 
-	Toast.prototype.__defineSetter__("backgroundColor", function(color) {
-		this._bgColor = color;
-		this.style.textContent = setStyle.call(this);
-	});
+    get DEFAULT_DURATION() {
+        return this.ToastConstants.DEFAULT_DURATION;
+    }
 
-	Toast.prototype.__defineSetter__("color", function(color) {
-		this._color = color;
-		this.style.textContent = setStyle.call(this);
-	});
+    set duration(t) {
+        t = parseInt(t);
+        if (t <= 0) throw Error("Error: Toast duration must be a positive integer");
+        this._duration = t;
+    }
 
-	Toast.makeText = function({duration:duration, text:text, position:position, backgroundColor: backgroundColor, color: color}) {
-		toast.duration = parseInt(duration) || Toast.DEFAULT_DURATION;
-		toast.position = typeof(position === "undefined") ? ToastConstants.BOTTOM : position;
-		toast.text = text;
-		toast.backgroundColor = backgroundColor || "#f0f0f0";
-		toast.color = color || null;
-		return toast;
-	}
+    set position(pos) {
+        if (pos !== ToastConstants.BOTTOM && pos !== ToastConstants.TOP)
+            throw new Error("Error: Toast position must be equal to Toast.BOTTOM or Toast.TOP");
+        this._position = pos;
+        this.style.textContent = setStyle.call(this);
+    }
 
-	Toast.prototype.show = function(duration) {
-		if (duration) this.duration = duration;
-		this.toastElement.removeAttribute("style");
-		this.toastElement.removeAttribute("class");
-		this.toastElement.classList.add("fadein");
-		setTimeout(() => hide.call(this), this._duration+1000);
-	}
+    set text(text = "") {
+        this.toastElement.innerText = text;
+    }
 
-	const hide = function() {
-		this.toastElement.classList.remove("fadein");
-		this.toastElement.classList.add("fadeout");
-	}
+    set backgroundColor(color) {
+        this._bgColor = color;
+        this.style.textContent = setStyle.call(this);
+    }
 
-	const toast = new Toast();
-	window.Toast = Toast;
-})();
+    static makeText({ duration, text, position, backgroundColor, color }) {
+        Toast.toast.duration = parseInt(duration) || Toast.DEFAULT_DURATION;
+        Toast.toast.position = typeof position === "undefined" ? ToastConstants.BOTTOM : position;
+        Toast.toast.text = text;
+        Toast.toast.backgroundColor = backgroundColor || "#f0f0f0";
+        Toast.toast.color = color || null;
+        return Toast.toast;
+    }
+
+    show(duration) {
+        if (duration) this.duration = duration;
+        this.toastElement.removeAttribute("style");
+        this.toastElement.removeAttribute("class");
+        this.toastElement.classList.add("fadein");
+        setTimeout(() => hide.call(this), this._duration + 1000);
+    }
+}
+
+Toast.toast = new Toast();
